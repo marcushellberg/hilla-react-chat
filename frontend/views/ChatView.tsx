a7107cb7e3fc50734413ea2615c7d3b37b5dbc99
Marcus Hellberg <marcus@vaadin.com>
2023-01-10 12:36:20 -0700

    add wrapping div

--- frontend/views/ChatView.tsx
+++ frontend/views/ChatView.tsx
@@ -1,6 +1,10 @@
-import {TextFieldChangeEvent} from "@vaadin/text-field/src/vaadin-text-field.js";
-import {MessageInputSubmitEvent} from '@vaadin/message-input/src/vaadin-message-input.js'
+import {TextFieldChangeEvent} from '@vaadin/text-field/src/vaadin-text-field.js';
+import {MessageInputSubmitEvent} from '@vaadin/message-input/src/vaadin-message-input.js';
 
 export function ChatView() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
 }
