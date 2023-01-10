5da8dc8ebe0308013ee6fe50612b9841f0eec99f
Marcus Hellberg <marcus@vaadin.com>
2023-01-10 12:32:45 -0700

    re-add event imports

--- frontend/views/ChatView.tsx
+++ frontend/views/ChatView.tsx
@@ -1,3 +1,6 @@
+import {TextFieldChangeEvent} from "@vaadin/text-field/src/vaadin-text-field.js";
+import {MessageInputSubmitEvent} from '@vaadin/message-input/src/vaadin-message-input.js'
+
 export function ChatView() {
   return <h1>Hello, world!</h1>;
 }
