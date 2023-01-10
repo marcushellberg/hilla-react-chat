1b675bc7159deeb62df65dff079a9dcc8f3aafc8
Marcus Hellberg <marcus@vaadin.com>
2023-01-10 12:28:58 -0700

    Remove extra input

--- frontend/views/ChatView.tsx
+++ frontend/views/ChatView.tsx
@@ -1,10 +1,3 @@
-import {MessageList} from "@hilla/react-components/MessageList.js";
-import {TextFieldChangeEvent} from "@vaadin/text-field/src/vaadin-text-field.js";
-import {MessageInputSubmitEvent} from '@vaadin/message-input/src/vaadin-message-input.js'
-
 export function ChatView() {
-  
-  return (
-    <h1>Hello, world!</h1>
-  )
+  return <h1>Hello, world!</h1>;
 }
